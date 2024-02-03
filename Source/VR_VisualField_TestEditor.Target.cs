0d1ee5880990c28da720346ae426f43379aa4a9a
jwmcfar2 <jwmcfar2@ncsu.edu>
2024-02-03 11:29:39 +0000

    initial push _JMac

---
+++ Source/VR_VisualField_TestEditor.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class VR_VisualField_TestEditorTarget : TargetRules
+{
+	public VR_VisualField_TestEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "VR_VisualField_Test" } );
+	}
+}
